2a26f8c5e5f32181565d907f4142793d368cfa9d
alviennn <alvien.rnp@gmail.com>
2024-04-02 21:32:45 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+video.addEventListener("click",function video(){
+    alert("Ini video clipnya");
+});
+
+audio.addEventListener("click",function audio(){
+    alert("Ini lagunya");
+});
+
+gambar.addEventListener("click",function gambar(){
+    alert("Ini fotonya");
+});
